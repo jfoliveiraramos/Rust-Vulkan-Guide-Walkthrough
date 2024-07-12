edfeef217599e84a9c9df545c95f12e769e41254
João Ramos <1joaoramos1@gmail.com>
2024-07-12 16:19:09 +0100

    Implements logic to setup a Vulkan Instance

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,53 @@
     clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
+use vulkanalia::Version;
+
+use anyhow::{anyhow, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
+
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
-use winit::window::{Window, WindowBuilder};
+use winit::window::{self, Window, WindowBuilder};
 
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> VkResult<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Rust Vulkan Guide\n")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let flags = if 
+        cfg!(target_os = "macos") && 
+        entry.version()? >= PORTABILITY_MACOS_VERSION 
+    {
+        info!("Enabling extensions for macOS portability.");
+        extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    entry.create_instance(&info, None)
+}
 fn main() -> Result<()> {
     pretty_env_logger::init();
 
@@ -43,18 +84,26 @@ fn main() -> Result<()> {
 }
 
 #[derive(Clone, Debug)]
-struct App {}
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
 impl App {
     unsafe fn create(window: &Window) -> Result<Self> {
-        Ok(Self {})
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
 
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
         Ok(())
     }
 
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None);
+    }
 }
 
 #[derive(Clone, Debug, Default)]
